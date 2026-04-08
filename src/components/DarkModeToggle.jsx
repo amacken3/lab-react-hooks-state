0ed514373b0e426352092a945908d419c8d54d9b
Aengus MacKenzie <aengus.2000@gmail.com>
2026-04-08 14:10:18 -0400

    fixed syntax

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
 const DarkModeToggle = ({ darkMode, toggleDarkMode }) => {
-  // TODO: Implement dark mode toggle logic
-
   return (
     <button onClick={toggleDarkMode}>{darkMode ? 'Toggle Light Mode' : 'Toggle Dark Mode'}</button>
   )
